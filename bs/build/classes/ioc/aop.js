b6758ea806f75207ad02d52b124d70d5ad0562fb
xujian <xu_jian03@qq.com>
2019-03-21 12:34:29 +0800

    提交

---
+++ bs/build/classes/ioc/aop.js
@@ -0,0 +1,42 @@
+var ioc = {
+	txNONE : {
+            type : 'org.nutz.aop.interceptor.TransactionInterceptor',
+            args : [0]
+    },
+    txREAD_UNCOMMITTED : {
+            type : 'org.nutz.aop.interceptor.TransactionInterceptor',
+            args : [1]
+    },
+    txREAD_COMMITTED : {
+            type : 'org.nutz.aop.interceptor.TransactionInterceptor',
+            args : [2]
+    },
+    txREPEATABLE_READ : {
+            type : 'org.nutz.aop.interceptor.TransactionInterceptor',
+            args : [4]
+    },
+    txSERIALIZABLE : {
+            type : 'org.nutz.aop.interceptor.TransactionInterceptor',
+            args : [8]
+    },
+    //声明一个log进行日志记录
+    log : {
+            type :'org.nutz.aop.interceptor.LoggingMethodInterceptor'
+    },
+    $aop : {
+        type : 'org.nutz.ioc.aop.config.impl.ComboAopConfigration',
+        fields : {
+                aopConfigrations  : [
+                        {       type : 'org.nutz.ioc.aop.config.impl.JsonAopConfigration',
+                                fields : {
+                                itemList : [
+                                            ['net\\.wendal\\..+','.+','ioc:log'],
+                                            ['net\\.wendal\\.nutz\\..+','.+','ioc:txSERIALIZABLE']
+                                ]
+                                }
+                        },
+                        {       type : 'org.nutz.ioc.aop.config.impl.AnnotationAopConfigration'}
+                ]
+        }
+    }
+};
